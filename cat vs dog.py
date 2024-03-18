583cd1fe87acddcc515ad40031d270f4a9fc619a
areesha7 <121893993+areesha7@users.noreply.github.com>
2024-03-18 23:16:58 +0500

    Add files via upload

---
+++ cat vs dog.py
@@ -0,0 +1,552 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install opencv-python')
+
+
+# In[3]:
+
+
+import requests
+
+url = "https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip"
+output_path = "/tmp/cats_and_dogs_filtered.zip"
+
+response = requests.get(url)
+with open(output_path, "wb") as f:
+    f.write(response.content)
+
+print("Download completed.")
+
+
+# In[5]:
+
+
+import os
+import zipfile
+
+local_zip = '/tmp/cats_and_dogs_filtered.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+
+# In[6]:
+
+
+base_dir = '/tmp/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+
+# In[7]:
+
+
+train_cat_fnames = os.listdir(train_cats_dir)
+print(train_cat_fnames[:10])
+
+train_dog_fnames = os.listdir(train_dogs_dir)
+train_dog_fnames.sort()
+print(train_dog_fnames[:10])
+
+
+# In[8]:
+
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))
+print('total training dog images:', len(os.listdir(train_dogs_dir)))
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Parameters for our graph; we'll output images in a 4x4 configuration
+nrows = 4
+ncols = 4
+
+# Index for iterating over images
+pic_index = 0
+
+
+# In[11]:
+
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+fig = plt.gcf()
+fig.set_size_inches(ncols * 4, nrows * 4)
+
+pic_index += 8
+next_cat_pix = [os.path.join(train_cats_dir, fname) 
+                for fname in train_cat_fnames[pic_index-8:pic_index]]
+next_dog_pix = [os.path.join(train_dogs_dir, fname) 
+                for fname in train_dog_fnames[pic_index-8:pic_index]]
+
+for i, img_path in enumerate(next_cat_pix+next_dog_pix):
+  # Set up subplot; subplot indices start at 1
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off') # Don't show axes (or gridlines)
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+
+# In[10]:
+
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+
+# In[11]:
+
+
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+
+# In[12]:
+
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(1, activation='sigmoid')(x)
+
+# Create model:
+# input = input feature map
+# output = input feature map + stacked convolution/maxpooling layers + fully 
+# connected layer + sigmoid output layer
+model = Model(img_input, output)
+
+
+# In[13]:
+
+
+model.summary()
+
+
+# In[14]:
+
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+
+# In[15]:
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 20 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+# In[19]:
+
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,  # 2000 images = batch_size * steps
+      epochs=15,
+      validation_data=validation_generator,
+      validation_steps=50,  # 1000 images = batch_size * steps
+      verbose=2)
+
+
+# In[20]:
+
+
+import numpy as np
+import random
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+# Let's define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+visualization_model = Model(img_input, successive_outputs)
+
+# Let's prepare a random input image of a cat or dog from the training set.
+cat_img_files = [os.path.join(train_cats_dir, f) for f in train_cat_fnames]
+dog_img_files = [os.path.join(train_dogs_dir, f) for f in train_dog_fnames]
+img_path = random.choice(cat_img_files + dog_img_files)
+
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# Rescale by 1/255
+x /= 255
+
+# Let's run our image through our network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so can have them as part of our plot
+layer_names = [layer.name for layer in model.layers[1:]]
+
+# Now let's display our representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+  if len(feature_map.shape) == 4:
+    # Just do this for the conv / maxpool layers, not the fully-connected layers
+    n_features = feature_map.shape[-1]  # number of features in feature map
+    # The feature map has shape (1, size, size, n_features)
+    size = feature_map.shape[1]
+    # We will tile our images in this matrix
+    display_grid = np.zeros((size, size * n_features))
+    for i in range(n_features):
+      # Postprocess the feature to make it visually palatable
+      x = feature_map[0, :, :, i]
+      x -= x.mean()
+      x /= x.std()
+      x *= 64
+      x += 128
+      x = np.clip(x, 0, 255).astype('uint8')
+      # We'll tile each filter into this big horizontal grid
+      display_grid[:, i * size : (i + 1) * size] = x
+    # Display the grid
+    scale = 20. / n_features
+    plt.figure(figsize=(scale * n_features, scale))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+
+# In[22]:
+
+
+# Retrieve a list of accuracy results on training and validation data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and validation data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+
+
+# In[23]:
+
+
+import os
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+
+# In[22]:
+
+
+import requests
+
+url = 'https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+output_path = '/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+response = requests.get(url, allow_redirects=True)
+open(output_path, 'wb').write(response.content)
+
+print("File downloaded successfully!")
+
+
+# In[24]:
+
+
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+local_weights_file = '/tmp/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+pre_trained_model = InceptionV3(
+    input_shape=(150, 150, 3), include_top=False, weights=None)
+pre_trained_model.load_weights(local_weights_file)
+
+
+# In[25]:
+
+
+for layer in pre_trained_model.layers:
+  layer.trainable = False
+
+
+# In[26]:
+
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape:', last_layer.output_shape)
+last_output = last_layer.output
+
+
+# In[27]:
+
+
+from tensorflow.keras.optimizers import RMSprop
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+# Add a dropout rate of 0.2
+x = layers.Dropout(0.2)(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(1, activation='sigmoid')(x)
+
+# Configure and compile the model
+model = Model(pre_trained_model.input, x)
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.0001),
+              metrics=['acc'])
+
+
+# In[28]:
+
+
+import os
+import zipfile
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+local_zip = '/tmp/cats_and_dogs_filtered.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+# Define our example directories and files
+base_dir = '/tmp/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# Note that the validation data should not be augmented!
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir, # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 20 using val_datagen generator
+validation_generator = val_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+
+# In[29]:
+
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=2,
+      validation_data=validation_generator,
+      validation_steps=50,
+      verbose=2)
+
+
+# In[30]:
+
+
+from tensorflow.keras.optimizers import SGD
+
+unfreeze = False
+
+# Unfreeze all models after "mixed6"
+for layer in pre_trained_model.layers:
+    if unfreeze:
+        layer.trainable = True
+    if layer.name == 'mixed6':
+        unfreeze = True
+
+# As an optimizer, here we will use SGD 
+# with a very low learning rate (0.00001)
+model.compile(loss='binary_crossentropy',
+              optimizer=SGD(
+                  lr=0.00001, 
+                  momentum=0.9),
+              metrics=['acc'])
+
+
+# In[30]:
+
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=50,
+      validation_data=validation_generator,
+      validation_steps=50,
+      verbose=2)
+
+
+# In[31]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Retrieve a list of accuracy results on training and validation data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and validation data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+
+
+# In[31]:
+
+
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+def classify_image(image_path, model):
+    # Load and preprocess the image
+    img = image.load_img(image_path, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0  # Normalize pixel values
+    
+    # Perform classification
+    prediction = model.predict(img_array)
+    if prediction[0] < 0.5:
+        return "Cat"
+    else:
+        return "Dog"
+
+
+# In[34]:
+
+
+image_path = "C:/tmp/cats_and_dogs_filtered/train/dogs/dog.14.jpg"
+predicted_class = classify_image(image_path, model)
+print("Predicted class:", predicted_class)
+
+
+# In[ ]:
+
+
+
+
